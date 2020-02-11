012fb5f16ba17ab39efd53996a2df27df6a5b2e6
kaizhao <kaizhao@E2227GW10.WIN2.CS.KSU.EDU>
2020-02-11 16:26:19 -0600

    read data from excel

--- AWS/src/InputData.java
+++ AWS/src/InputData.java
@@ -1,22 +1,82 @@
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+//import test3.Test3.stationInfo;
 
 public class InputData {
-	
-	public static String[] readTextFile(String text)throws Exception{
-		FileReader fr = new FileReader("C:\\" + text);
-		int i;
-		String file;
-		StringBuilder sb = new StringBuilder();
-		while( (i = fr.read()) != -1){
-			sb = sb.append((char)i);					
-		}
-		fr.close();
-		
-		file = sb.toString();
-		String[] data = file.split(",");		
-		return data;
-	}
 
+	XSSFWorkbook xwb;
+	String filePath;
+	ArrayList<stationInfo> allData = new ArrayList<>();
+	
+	class stationInfo{
+		String state;
+		String county;
+		String name; 
+		String[] data;
+		
+		stationInfo(String s, String c, String n, String[] d ){
+			state = s;
+			county = c;
+			name = n;
+			data = d;
+		}
+	}
+	
+	public InputData(String path) throws IOException {
+		filePath = path;
+		xwb = new XSSFWorkbook(filePath);
+	}
+	
+	public void readSheet(String sheetName) {
+		
+    	try {    		
+    	    XSSFSheet sheet = xwb.getSheet(sheetName);	     
+    	    XSSFRow row;
+    	    XSSFCell cell;  	     
+    	     for(int i = 0; i < sheet.getLastRowNum(); i++ ) {
+    	    	row = sheet.getRow(i); 	
+    	    	stationInfo element;
+    	    	String state = row.getCell(0).toString();
+	    		String county = row.getCell(1).toString();
+	    		String name = row.getCell(2).toString();
+	    		String[] data = new String[row.getLastCellNum()-3];
+    	    	for(int j = 3; j <row.getLastCellNum(); j ++) {
+    	    		data[j-3] = row.getCell(j).toString();	    		
+    	    	}
+    	    	element = new stationInfo(state, county, name, data);
+    	    	allData.add(element);   	    	 
+    	     }		 
+    	}catch(Exception e) {
+    		 e.printStackTrace();
+    	}  	
+	}
+	
+	public ArrayList<stationInfo> filterByState(String stateName, ArrayList<stationInfo> upperLevelData) {
+		ArrayList<stationInfo> list = new ArrayList<>();
+		for(stationInfo ele : upperLevelData) {
+			if(ele.state.equals(stateName)) {
+				list.add(ele);
+			}
+		}		
+		return list;
+	}
+	
+	public ArrayList<stationInfo> filterByCounty(String countyName, ArrayList<stationInfo> upperLevelData) {
+		ArrayList<stationInfo> list = new ArrayList<>();
+		for(stationInfo ele : upperLevelData) {
+			if(ele.county.equals(countyName)) {
+				list.add(ele);
+			}
+		}		
+		return list;
+	}
 
 	
 }
--- AWS/src/Main.java
+++ AWS/src/Main.java
@@ -1,8 +1,10 @@
 import java.io.FileReader;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+
 
 
 public class Main extends JFrame {
@@ -11,6 +13,23 @@ public class Main extends JFrame {
 	public static void main(String[] args) throws Exception {
 
 		//String[] data = InputData.readTextFile("test.txt");
+		
+		String path = "C:/Users/kaizhao/Desktop/AWSdata.xlsx";
+    	InputData example = new InputData(path);
+    	example.readSheet("Climate");
+
+    	
+    	
+    	ArrayList<InputData.stationInfo> stateList = example.filterByState("AK", example.allData);
+
+    	ArrayList<InputData.stationInfo> countyList = example.filterByCounty("ALASKA", stateList);
+
+    	
+    	for(InputData.stationInfo ele : countyList) {
+    		System.out.println(ele.name);
+    	}
+    	
+		
 		
 		
 		MainFrame frame = new MainFrame();
